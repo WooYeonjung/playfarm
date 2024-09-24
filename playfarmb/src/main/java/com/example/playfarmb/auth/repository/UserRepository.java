627d3a64ee20d80e2314a90e63c4fb4816343f68
Ellie <shyeounjung@gmail.com>
2024-09-24 10:45:16 +0900

    repository, service, serviceImpl

---
+++ playfarmb/src/main/java/com/example/playfarmb/auth/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.example.playfarmb.auth.repository;
+
+public class UserRepository {
+
+}
---
+++ playfarmb/src/main/java/com/example/playfarmb/auth/service/UserService.java
@@ -0,0 +1,5 @@
+package com.example.playfarmb.auth.service;
+
+public interface UserService {
+
+}
