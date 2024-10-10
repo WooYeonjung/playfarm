570e16e5b2299e9eb714fa8f08fb885518243ef3
ye0o0n <jaeyeon2707@gmail.com>
2024-10-10 16:51:41 +0900

    Revert "현제 작업들"
    This reverts commit e40f50c1f3922f34fd0c80ebc01a4b77c8a96f4e.

--- playfarmb/src/main/java/com/example/playfarmb/support/inquiry/repository/InquiryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.playfarmb.support.inquiry.repository;
-
-import com.example.playfarmb.support.inquiry.entity.Inquiry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
-
-}
